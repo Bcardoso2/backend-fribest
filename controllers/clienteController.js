f8d5a7a804fa8ff45a94bf3128c056e1468e1aeb
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2025-01-29 16:42:23 -0300

    Update clienteController.js att

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -20,6 +20,11 @@ const getAllClientes = async (req, res) => {
 
 const createCliente = async (req, res) => {
   try {
+    // Verificar se o CPF está vazio e transformar em null
+    if (req.body.cpf === '') {
+      req.body.cpf = null;
+    }
+
     const cliente = await Cliente.create(req.body);
     res.status(201).json(cliente);
   } catch (error) {
@@ -31,6 +36,7 @@ const createCliente = async (req, res) => {
       console.error('Erros de validação:', validationErrors);
     }
 
+    // Resposta de erro com detalhes específicos
     res.status(500).json({
       error: 'Erro ao criar cliente',
       details: error.name === 'SequelizeValidationError' ? validationErrors : error.message, // Detalhes específicos para erro de validação
