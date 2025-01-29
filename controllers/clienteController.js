ad51d55ab2c07a6d80d90883ce63ae87048de53a
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2025-01-29 16:32:11 -0300

    Update clienteController.js

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,29 +1,16 @@
 const Cliente = require('../models/Cliente.js');
 
-const getAllClientes = async (req, res) => {
-  try {
-    const { funcionario_id = null } = req.query;
-
-    const clientes = await Cliente.findAll({
-      where: funcionario_id ? { funcionario_id } : {},
-    });
-
-    res.json(clientes);
-  } catch (error) {
-    console.error('Erro ao buscar clientes:', error); // Log do erro
-    res.status(500).json({
-      error: 'Erro ao buscar clientes',
-      details: error.message, // Detalhes adicionais para ajudar na depuração
-    });
-  }
-};
-
 const createCliente = async (req, res) => {
   try {
+    // Verificar e tratar CPF vazio
+    if (req.body.cpf === '') {
+      req.body.cpf = null;
+    }
+
     const cliente = await Cliente.create(req.body);
     res.status(201).json(cliente);
   } catch (error) {
-    console.error('Erro ao criar cliente:', error); // Log do erro
+    console.error('Erro ao criar cliente:', error);
 
     // Logando detalhes do erro de validação
     if (error.name === 'SequelizeValidationError') {
@@ -33,10 +20,10 @@ const createCliente = async (req, res) => {
 
     res.status(500).json({
       error: 'Erro ao criar cliente',
-      details: error.name === 'SequelizeValidationError' ? validationErrors : error.message, // Detalhes específicos para erro de validação
+      details: error.name === 'SequelizeValidationError' ? validationErrors : error.message,
     });
   }
 };
 
-// Exportando as funções getAllClientes e createCliente
-module.exports = { getAllClientes, createCliente };
+// Exportando as funções
+module.exports = { createCliente };
